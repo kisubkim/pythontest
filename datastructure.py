095f0d4e1a7291d37d1374a14bd6e6074b0ea323
WhiteBear <zxkiki@outlook.kr>
2025-02-12 23:49:09 +0900

    기존에 python 공부했던 내용 upload
    기존 공부내용 모두 upload

---
+++ datastructure.py
@@ -0,0 +1,46 @@
+# my_list = [10, 20, 30, 40]
+# print(my_list)
+
+# my_list.append(50)
+# print(my_list)
+
+# print(len(my_list))
+
+# test = my_list.pop()
+# print(test)
+
+# print(my_list)
+
+# 리스트 정렬 예시
+my_list = [40, 10, 30, 20]
+print("Original list:", my_list)
+
+# 오름차순 정렬
+my_list.sort()
+print("Sorted list (ascending):", my_list)
+
+# 내림차순 정렬
+my_list.sort(reverse=True)
+print("Sorted list (descending):", my_list)
+
+# 리스트 연산자 예제
+# 리스트 연산자 예제
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+# 리스트 합치기
+combined_list = list1 + list2
+print("Combined list:", combined_list)
+
+# 리스트 반복
+repeated_list = list1 * 3
+print("Repeated list:", repeated_list)
+
+# 리스트 포함 여부 확인
+is_in_list = 2 in list1
+print("Is 2 in list1?:", is_in_list)
+
+# 리스트 길이
+list_length = len(list1)
+print("Length of list1:", list_length)
+
